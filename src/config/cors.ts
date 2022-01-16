669d24eccf04d332c98ef9e198857fa7520dba05
Courtney Jooste <courtney@codelab.io>
2022-01-16 14:53:28 +0200

    Revert "chore: updated cors to include methods"
    This reverts commit a15fc9d16e7629051f8795ef80deb3709180b21c.

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,4 @@
 export const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'api_key'],
+  origin: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'api_key'],
 };
