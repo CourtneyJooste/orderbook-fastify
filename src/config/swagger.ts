b8581969a226d47df0bee8646084060b6cbd873c
Courtney Jooste <courtney@codelab.io>
2022-01-16 14:24:00 +0200

    chore: updated swagger config

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,14 +1,15 @@
 export const Options = {
   routePrefix: '/docs',
   exposeRoute: true,
+  enableCors: true,
   swagger: {
     info: {
       title: 'Orderbook Fastify API',
       description: 'A fast, in-memory order book to create and fulfil limit orders',
       version: '1.0.0',
     },
-    host: 'localhost:8080',
-    schemes: ['http'],
+    host: `${process.env.HOST || 'localhost'}:${process.env.PORT || '8080'}`,
+    schemes: ['http', 'https'],
     consumes: ['application/json'],
     produces: ['application/json'],
   },
