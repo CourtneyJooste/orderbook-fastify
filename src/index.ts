01452e12d9c72b92b206d5862409480aafdc9b0c
Courtney Jooste <courtney@codelab.io>
2022-01-16 13:59:40 +0200

    chore: added address to server index

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ routes.forEach(route => {
 });
 
 const start = async (): Promise<void> => {
-  server.listen(process.env.PORT || 8080, (err, address) => {
+  server.listen(process.env.PORT || 8080, '0.0.0.0', (err, address) => {
     if (err) {
       console.error(err);
       process.exit(1);
